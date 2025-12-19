05a472c24b179d2186d6616215ae0d2e778c77b7
Neal-Granal <108686430+Neal-Granal@users.noreply.github.com>
2025-12-18 20:12:47 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    // ⚠️ ATTENTION : Sur un site public, cette clé peut être volée.
+    // Pour Github Pages, c'est acceptable pour un prototype, mais risqué à long terme.
+    API_KEY: "AIzaSyCMfqbhNp1vVVP6gNjNchr_veh7PjyFePI", 
+    
+    // Réglages du livre
+    BOOK: {
+        width: 480,
+        height: 720,
+        minWidth: 315,
+        maxWidth: 650,
+        minHeight: 450,
+        maxHeight: 950
+    },
+    
+    // Réglages écriture (machine à écrire)
+    TYPING_SPEED: 30
+};
